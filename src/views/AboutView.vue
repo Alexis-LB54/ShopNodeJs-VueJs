f1e66c0bebac2ac9348f96d6d1afce35b7024909
Alexis-LB54 <alexis@lebail.eu>
2022-05-09 16:50:43 +0200

    modif about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,27 @@
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <form @submit.prevent="login">
+      <p>Veuillez choisir un avatar:</p>
+      <input type="hidden" name="MAX_FILE_SIZE" value="250000" />
+      <input type="file" name="fic" size="50" />
+      <input type="submit" value="choisir" />
+    </form>
   </div>
 </template>
 
-<style>
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+import { useTokenStore } from "../stores/token";
+import { useRoute, useRouter } from "vue-router";
+
+const Storedutoken = useTokenStore();
+const router = useRouter();
+
+const profile = ref({
+  photo: "",
+});
+</script>
+
+<style scoped>
 </style>
