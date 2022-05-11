b3e2d89ba0813b4c0bb5eaa6989c9f3b151cf7aa
Alexis-LB54 <alexis@lebail.eu>
2022-05-11 10:11:22 +0200

    ajout de l'image dans la bdd

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,27 +1,70 @@
-
 <template>
-  <div>
-    <form @submit.prevent="login">
-      <p>Veuillez choisir un avatar:</p>
-      <input type="hidden" name="MAX_FILE_SIZE" value="250000" />
-      <input type="file" name="fic" size="50" />
-      <input type="submit" value="choisir" />
-    </form>
-  </div>
+  <h1>Register</h1>
+  <form @submit.prevent="handleSubmit">
+    <label for="username">username</label>
+    <input type="text" name="username" id="username" v-model="user.username" />
+    <label for="password">Password</label>
+    <input
+      type="password"
+      name="password"
+      id="password"
+      v-model="user.password"
+    />
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email" v-model="user.email" />
+    <label for="image">Avatar</label>
+    <input
+      type="file"
+      name="image"
+      id="image"
+      accept="image/"
+      @change="onFileChange"
+      ref="image"
+    />
+    <input type="submit" value="Register" />
+  </form>
+  <p v-if="response !== ''">
+    {{ response }} <router-link to="/">Log in to your account</router-link>
+  </p>
 </template>
-
-<script setup lang="ts">
-import { ref } from "@vue/reactivity";
-import { useTokenStore } from "../stores/token";
-import { useRoute, useRouter } from "vue-router";
-
-const Storedutoken = useTokenStore();
-const router = useRouter();
-
-const profile = ref({
-  photo: "",
-});
+<script>
+export default {
+  data() {
+    return {
+      user: {},
+      response: "",
+    };
+  },
+  mounted() {
+    sessionStorage.clear();
+  },
+  methods: {
+    onFileChange: function () {
+      const image = this.$refs.image.files[0];
+      this.user.image = image;
+    },
+    handleSubmit: async function () {
+      JSON.stringify(this.user.image);
+      let formData = new FormData();
+      formData.append("username", this.user.username);
+      formData.append("email", this.user.email);
+      formData.append("password", this.user.password);
+      formData.append("image", this.user.image);
+      let res = await fetch("http://localhost:96/inscription", {
+        method: "POST",
+        body: formData,
+      })
+        .then((r) => r.json())
+        .catch((e) => console.log(e));
+      console.log(res);
+      if (res.error) {
+        this.response = res.message;
+        return;
+      }
+      this.response = "Registered successfully !";
+    },
+  },
+};
 </script>
-
 <style scoped>
 </style>
